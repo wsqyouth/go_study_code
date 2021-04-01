4b63999daa3c78061549b7c71851d5003dc8168c
wsqyouth <2631520922@qq.com>
2021-04-01 21:16:29 +0800

    0401_测试指针tag
    Signed-off-by: wsqyouth <2631520922@qq.com>

---
+++ emptyJson/useJson.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type CrtInfo struct {
+	TagType *uint32 `json:"use_button,omitempty"`
+}
+
+func main() {
+	var crtInfo CrtInfo
+	defaultUint32 := new(uint32)
+	crtInfo.TagType = defaultUint32
+	tagType := uint32(1)
+	crtInfo.TagType = &tagType
+	b, e := json.Marshal(crtInfo)
+	if e != nil {
+		fmt.Println("marshal error")
+	}
+	fmt.Println(string(b))
+}
