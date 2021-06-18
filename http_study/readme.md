cdb116f168c1fa2b4c0e1ba35fca84ad28e8c1a9
wsqyouth <2631520922@qq.com>
2021-06-18 17:26:42 +0800

    feature: 0618_study
    Signed-off-by: wsqyouth <2631520922@qq.com>

---
+++ http_study/readme.md
@@ -0,0 +1,3 @@
+### 参考文档
+
+https://zetcode.com/all/#go
--- strings_study/string_op.go
+++ strings_study/string_op.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	strSplit()
+	strToFloat()
+}
+
+func strSplit() {
 	orignSubID := "10001010;10000838"
 	var dstID string
 	slices := strings.SplitN(orignSubID, ";", 3)
@@ -13,4 +19,12 @@ func main() {
 		dstID = "0;" + slices[1]
 	}
 	fmt.Println(dstID)
+}
+func strToFloat() {
+	str := "12.345"
+	num, err := strconv.ParseFloat(str, 10)
+	if err != nil {
+		fmt.Printf("strtofloat err. err: %v", err)
+	}
+	fmt.Printf("%T  %v\n", num, num)
 }
