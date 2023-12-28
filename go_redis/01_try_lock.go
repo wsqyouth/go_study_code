051876897480b0c3223356d89e81ff01edcaca07
wsqyouth <2631520922@qq.com>
2023-12-28 15:34:35 +0800

    feat: 分布式锁优化
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- go_redis/01_try_lock.go
+++ go_redis/01_try_lock.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math"
 	"sync"
 	"time"
@@ -23,11 +24,11 @@ func TryLock(ctx context.Context, resource string, owner string) (func(), error)
 	if deadline, ok := ctx.Deadline(); ok {
 		timeout = time.Until(deadline)
 		if timeout <= 0 {
-			// 已超时则不用加锁
+			log.Printf("context deadline exceeded, not attempting to acquire lock")
 			return nil, nil
 		}
 	}
-	// 打印检查timeout是否在变化
+
 	fmt.Printf("timeout: %v\n", timeout)
 	ok, err := redisClient.SetNX(ctx, key, owner, time.Duration(math.Ceil(timeout.Seconds()))*time.Second).Result()
 	if err != nil {
@@ -36,12 +37,13 @@ func TryLock(ctx context.Context, resource string, owner string) (func(), error)
 	if !ok {
 		var lockBy string
 		if err := redisClient.Get(ctx, key).Scan(&lockBy); err == nil && lockBy == owner {
+			// Lock is already held by this owner
 			return nil, nil
 		}
-		// 打印检查锁被谁持有了
 		fmt.Printf("newowner: %v, lockBy:%v\n", owner, lockBy)
-		return nil, errors.New(fmt.Sprintf("lock found, key: %s", key))
+		return nil, errors.Errorf("lock already held, key: %s, by: %s", key, lockBy)
 	}
+
 	return func() {
 		if _, err := redisClient.Del(ctx, key).Result(); err != nil {
 			fmt.Printf("unlock failed, key: %s, err: %v\n", key, err)
@@ -50,6 +52,7 @@ func TryLock(ctx context.Context, resource string, owner string) (func(), error)
 }
 
 var counter int
+var counterMutex sync.Mutex // 用于保护 counter 变量
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -60,10 +63,11 @@ func main() {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
-
-			unlock, err := TryLock(ctx, "resource1", fmt.Sprintf("owner%d", id))
+			owner := fmt.Sprintf("owner%d", id) //// 生成一个owner ID
+			unlock, err := TryLock(ctx, "resource1", owner)
 			if err != nil {
 				fmt.Printf("lock failed: %v\n", err)
+				fmt.Printf("[%s] lock failed: %v\n", owner, err)
 				return
 			}
 			if unlock != nil {
@@ -71,7 +75,9 @@ func main() {
 			}
 
 			// do something with the locked resource
+			counterMutex.Lock()
 			counter++
+			counterMutex.Unlock()
 			fmt.Printf("Goroutine %d incremented the counter. Counter: %d\n", id, counter)
 		}(i)
 	}
