71e4cb101f58a3891aa9e96afc040b6fcc3701c5
wsqyouth <2631520922@qq.com>
2024-01-24 11:04:59 +0800

    feat: go创建协程二次封装
    Signed-off-by: wsqyouth <2631520922@qq.com>

---
+++ go_routine/04_process_panic_routine.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"runtime/debug"
+	"time"
+)
+
+func main() {
+	// 当panicDemo函数触发panic时，Go函数会捕获到panic并使用log.Printf记录错误信息。
+	Go(context.Background(), panicDemo)
+	time.Sleep(time.Second * 10)
+	fmt.Println("hello")
+}
+
+func panicDemo() {
+	var x, y = 1, 0
+	defer fmt.Println("exits for panicking:", x)
+	x = x / y // will panic
+	x++       // unreachable
+}
+
+// Go calls the given function in a new goroutine. panic is recovered.
+func Go(ctx context.Context, f func()) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("panic: %v, stacktrace: %s", r, string(debug.Stack()))
+			}
+		}()
+
+		f()
+	}()
+}
+
+/*
+使用原始关键字go开启goroutine可能会有panic，因此需要对其封装，后续可以使用这个进行保护
+*/
